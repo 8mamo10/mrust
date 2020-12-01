3feb413c9e584bf09fd4f335026208e662520334
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-12-01 21:23:55 +0900

    Add port-scanner

---
+++ port-scanner/src/main.rs
@@ -0,0 +1,22 @@
+use pnet::packet::tcp;
+use std::net;
+
+struct PacketInfo {
+    my_ipaddr: net::Ipv4Addr,
+    target_ipaddr: net::Ipv4Addr,
+    my_port: u16,
+    maxinum_port: u16,
+    scan_type: ScanType,
+}
+
+#[derive(Copy, Clone)]
+enum ScanType {
+    SynScan = tcp::TcpFlags::SYN as isize,
+    FinScan = tcp::TcpFlags::FIN as isize,
+    XmasScan = (tcp::TcpFlags::FIN | tcp::TcpFlags::URG | tcp::TcpFlags::PSH) as isize,
+    NullScan,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
