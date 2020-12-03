cc1c81a11557bcae17a6d20c5776eb609ff1259c
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-12-04 07:44:33 +0900

    Enable specify command option

--- port-scanner/src/main.rs
+++ port-scanner/src/main.rs
@@ -1,11 +1,13 @@
 use pnet::packet::tcp;
-use std::{env, net};
+use std::{collections, env, fs, net};
+#[macro_use]
+extern crate log;
 
 struct PacketInfo {
     my_ipaddr: net::Ipv4Addr,
     target_ipaddr: net::Ipv4Addr,
     my_port: u16,
-    maxinum_port: u16,
+    maximum_port: u16,
     scan_type: ScanType,
 }
 
@@ -21,4 +23,37 @@ fn main() {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
     let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        error!("Bad number of arguments. [ipaddr][scantype]");
+        std::process::exit(1);
+    }
+    let packet_info = {
+        let contents = fs::read_to_string(".env").expect("Failed to read env file");
+        let lines: Vec<_> = contents.split('\n').collect();
+        let mut map = collections::HashMap::new();
+        for line in lines {
+            let elm: Vec<_> = line.split('=').map(str::trim).collect();
+            if elm.len() == 2 {
+                map.insert(elm[0], elm[1]);
+            }
+        }
+        PacketInfo {
+            my_ipaddr: map["MY_IPADDR"].parse().expect("invalid ipaddr"),
+            target_ipaddr: args[1].parse().expect("invalid target ipaddr"),
+            my_port: map["MY_PORT"].parse().expect("invalid port number"),
+            maximum_port: map["MAXIMUM_PORT_NUM"]
+                .parse()
+                .expect("invalid maximum port number"),
+            scan_type: match args[2].as_str() {
+                "sS" => ScanType::SynScan,
+                "sF" => ScanType::FinScan,
+                "sX" => ScanType::XmasScan,
+                "sN" => ScanType::NullScan,
+                _ => {
+                    error!("Undefined scan method, only accept [sF|sF|sX|sN].");
+                    std::process::exit(1);
+                }
+            },
+        }
+    };
 }
