f428bc536b6f44381a9cceaf09999e33bcd39808
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-12-22 21:51:51 +0900

    Query lease_entry

--- dhcp-server/src/database.rs
+++ dhcp-server/src/database.rs
@@ -1,4 +1,4 @@
-use rusqlite::Connection;
+use rusqlite::{params, Connection};
 use std::net::Ipv4Addr;
 
 pub fn select_addresses(
@@ -6,4 +6,20 @@ pub fn select_addresses(
     deleted: Option<u8>,
 ) -> Result<Vec<Ipv4Addr>, failure::Error> {
     Ok(Vec::new())
+}
+
+pub fn select_entry(
+    con: &Connection,
+    mac_addr: MacAddr,
+) -> Result<Option<Ipv4Addr>, failure::Error> {
+    let mut stmnt = con.prepare("SELECT ip_addr FROM lease_entries WHERE mac_addr = ?1")?;
+    let mut row = stmnt.query(params![mac_addr.to_string()])?;
+    if let Some(entry) = row.next()? {
+        let ip = entry.get(0)?;
+        let ip_string: String = ip;
+        Ok(Some(ip_string.parse()?))
+    } else {
+        info!("specified MAC addr was not found");
+        Ok(None)
+    }
 }
