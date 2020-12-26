9ad04ae1b238952096349fd797c80ada45d87493
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-12-26 15:20:57 +0900

    Judge the ip to be leased is available or not by icmp reply

--- dhcp-server/src/util.rs
+++ dhcp-server/src/util.rs
@@ -1,9 +1,59 @@
 use std::collections::HashMap;
 use std::io;
-use std::net::{AddrParseError, Ipv4Addr, UdpSocket};
+use std::net::{AddrParseError, IpAddr, Ipv4Addr, UdpSocket};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+use pnet::packet::icmp::echo_request::{EchoRequestPacket, MutableEchoRequestPacket};
+use pnet::packet::icmp::IcmpTypes;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::Packet;
+use pnet::transport::icmp_packet_iter;
+use pnet::transport::{self, TransportChannelType, TransportProtocol::Ipv4};
+use pnet::util::checksum;
 
 pub fn is_ipaddr_available(target_ip: Ipv4Addr) -> Result<(), failure::Error> {
-    Ok({})
+    let icmp_buf = create_default_icmp_buffer();
+
+    let icmp_packet = EchoRequestPacket::new(&icmp_buf).unwrap();
+    let (mut transport_sender, mut transport_receiver) = transport::transport_channel(
+        1024,
+        TransportChannelType::Layer4(Ipv4(IpNextHeaderProtocols::Icmp)),
+    )?;
+    transport_sender.send_to(icmp_packet, IpAddr::V4(target_ip))?;
+    let (sender, receiver) = mpsc::channel();
+    thread::spawn(move || {
+        let mut itr = icmp_packet_iter(&mut transport_receiver);
+        let (packet, _) = itr.next().unwrap();
+        if packet.get_icmp_type() == IcmpTypes::EchoReply {
+            match sender.send(true) {
+                Err(_) => {
+                    info!("icmp timeout!");
+                }
+                X => {
+                    return;
+                }
+            }
+        }
+    });
+    if receiver.recv_timeout(Duration::from_millis(200)).is_ok() {
+        let message = format!("ip addr is already in use: {}", target_ip);
+        warn!("{}", message);
+        Err(failure::err_msg(message))
+    } else {
+        debug!("not received reply within timeout");
+        Ok({})
+    }
+}
+
+fn create_default_icmp_buffer() -> [u8; 8] {
+    let mut buf = [0u8; 8];
+    let mut icmp_packet = MutableEchoRequestPacket::new(&mut buf).unwrap();
+    icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    let checksum = checksum(icmp_packet.to_immutable().packet(), 16);
+    icmp_packet.set_checksum(checksum);
+    buf
 }
 
 pub fn load_env() -> HashMap<String, String> {
