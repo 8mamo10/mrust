1a142f14a6db47ed88c1be741f21367300519a60
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-05-22 09:44:07 +0900

    Enable to open dialog

--- tauri/try-tauri/src/App.tsx
+++ tauri/try-tauri/src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api';
+import { open } from '@tauri-apps/api/dialog';
 
 function App() {
+
+  function executeCommands() {
+    invoke('simple_command');
+    invoke('command_with_message', {
+      message: 'some message'
+    }).then(message => console.log('command_with_message', message));
+    invoke('command_with_object', {
+      message: {
+        field_str: 'some message',
+        field_u32: 12
+      }
+    }).then(message => console.log('command_with_object', message));
+    for (let arg of [1, 2]) {
+      invoke('command_with_error', { arg }).then(message => {
+        console.log('command_with_error', message)
+      }).catch(message => {
+        console.log('command_with_error', message)
+      })
+    }
+    invoke('async_command', { arg: 14 }).then(message => {
+      console.log('async_command', message)
+    })
+  }
+  function openDialog() {
+    open().then(files => console.log(files))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +41,9 @@ function App() {
         </p>
         <br />
         Hello Tauri
+        <button onClick={executeCommands}>Click to execute command</button>
+        <br />
+        <button onClick={openDialog}>Click to open dialog</button>
         <a
           className="App-link"
           href="https://reactjs.org"
